9c469ea62cfdd83a863f6326b638f2b144ae28bd
yasmen <yasmen.khaled2000@gmail.com>
2024-02-01 22:09:47 +0200

    up

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,39 @@
+nx
+# update  
+apt-get update
+# install nginx
+apt-get install -y nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "<html>  
+<p> print hello </p>
+</html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+# server
+printf %s "server {
+    listen 80 default_server;
+        listen [::]:80 default_server;
+	    add_header X-Served-By $HOSTNAME;
+	        root   /var/www/html;
+		    index  index.html index.htm;
+
+		        location /hbnb_static {
+				alias /data/web_static/current;
+					index index.html index.htm;
+					    }
+
+					            location /redirect_me {
+						    	return 301 http://cuberule.com/;
+							    }
+
+							        error_page 404 /404.html;
+								    location /404 {
+								          root /var/www/html;
+									        internal;
+										    }
+									    }" > /etc/nginx/sites-available/default
+
+									    # restart nginx
+									    service nginx restart
