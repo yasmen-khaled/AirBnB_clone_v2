ccca2d5a55098fbffc99b2526e73f4676cca120d
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2024-02-01 23:48:55 +0300

    done

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,7 +3,7 @@
 
 from datetime import datetime
 from fabric.api import local
-import os.path
+import os.path as v
 
 
 
@@ -16,9 +16,11 @@ def do_pack():
                                                          x.hour,
                                                          x.minute,
                                                          x.second)
-    if os.path.isdir("versions") is False:
+    if v.isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
             return None
+            
     if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
+        
     return file
