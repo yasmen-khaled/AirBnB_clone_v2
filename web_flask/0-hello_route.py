18152366248aa662649ecc561fbebdad9d3eeff8
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2024-02-18 20:01:11 +0300

    test

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-        """Hello"""
-            return 'Hello HBNB!'
+    """returns"""
+    return 'Hello HBNB!'
 
-        if __name__ == '__main__':
-                app.run(host='0.0.0.0', port='5000')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
